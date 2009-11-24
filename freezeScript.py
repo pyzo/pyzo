2db158238e8e9073bbf20542922d1d51e6df4556
almar.klein <almar.klein@localhost>
2009-11-24 21:32:16 +0000

    - included freeze script - made title of window show the current file's filename when selection (well, showing to be precise) an editor. - made dropping of files possible, but this needs some more work if I want to do it nicely.   Also it doesn't work yet when dropping on the editor itself.

---
+++ freezeScript.py
@@ -0,0 +1,68 @@
+""" FREEZING WITH CX_FREEZE
+This script can be run as a script (no need to do distutils stuff...
+"""
+
+import sys, os, shutil
+from cx_Freeze import Executable, Freezer, setup
+
+# define app name and such
+name = "iep"
+baseDir = 'c:/projects/python/apps/'
+srcDir = 'c:/projects/python/iep2/'
+distDir = baseDir+name+'/'
+scriptFile = srcDir + 'iep.pyw'
+iconFile = srcDir + 'icon.ico'
+
+## includes and excludes
+
+# you usually do not need these
+excludes = ['_ssl', 'pyreadline', 'pdb', "email", 
+     "matplotlib", 'doctest', 
+    "scipy.linalg", "scipy.special", "Pyrex", 
+    "numpy.core._dotblas",
+    ]
+# excludes for tk
+tk_excludes = [ "pywin", "pywin.debugger", "pywin.debugger.dbgcon",
+                "pywin.dialogs", "pywin.dialogs.list",
+                "Tkconstants","Tkinter","tcl" ]
+excludes.extend(tk_excludes)
+excludes.append('numpy')
+
+
+includes = ['sip', "PyQt4.QtCore", "PyQt4.QtGui", 'PyQt4.Qsci'] # for qt to work
+
+
+## Go!
+# see http://cx-freeze.sourceforge.net/cx_Freeze.html for docs.
+
+# NOTE: I had tp add "import sys" to freezer.py for it to work and prevent
+# the "None object has no atribute modules" error message
+# The statement should be added right before "sys.modules[__name__] = m".
+
+# go there and enable importing from current directory
+os.chdir('c:/projects/python/iep2/')
+sys.path.append('')
+
+ex = Executable(    scriptFile, 
+                    icon=iconFile,
+                    appendScriptToExe = True,
+                    base = 'Win32GUI', # this is what hides the console
+                    )
+
+f = Freezer(    {ex:True}, 
+                includes = includes,
+                excludes = excludes,
+                targetDir = distDir,
+#                 copyDependentFiles = True,
+#                 appendScriptToExe=True,
+                optimizeFlag=2, 
+                compress=False,
+                silent=True,
+            )
+
+f.Freeze()
+
+# copy resource files
+shutil.copy(srcDir+'styles.ssdf', distDir+'styles.ssdf')
+for icon in ['icon16.png', 'icon32.png', 'icon48.png', 'icon.ico']:
+    shutil.copy(srcDir+icon, distDir+icon)
--- start.bat
+++
@@ -1,3 +0,0 @@
-"c:/program files/python31/python.exe" C:/projects/PYTHON/iep2/iep.py
-:"c:/program files/python31/python.exe" baseTextCtrl.py
-pause
