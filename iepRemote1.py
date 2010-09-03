532d298a7f919200ef3d723869f764d92392c7e7
almar.klein <almar.klein@localhost>
2010-09-03 18:19:04 +0200

    Startup script runs inside interpreter loop so interrupt works. Removed sys.excepthook again, because it will sometimes hook on exceptions that it should not handle. Not sure why. Left the code there in case I need to debug the interpreter.

--- iepRemote1.py
+++ iepRemote1.py
@@ -59,8 +59,9 @@ def iep_excepthook(type, value, tb):
             tb = tb.tb_next
     import time
     time.sleep(0.3) # Give some time for the message to be send
-    
-sys.excepthook = iep_excepthook
+
+# Uncomment to detect error in the interpreter itself
+# sys.excepthook = iep_excepthook
 
 
 ## Init interpreter and introspection tread
