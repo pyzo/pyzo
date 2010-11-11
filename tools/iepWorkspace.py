05cc4c4fb45bf6a11a7041c7191624f6be7781d0
Almar Klein <almarklein@users.noreply.github.com>
2010-11-11 23:30:47 +0100

    fixed bug in appending of .py -> should not happen for empty filenames. Made workspace tool work. Could do with some icons. I'd also like to enable expanding items, but I may leave that for the next releasem when Yoton is in place.

--- tools/iepWorkspace.py
+++ tools/iepWorkspace.py
@@ -9,118 +9,123 @@ tool_summary = "Lists the variables in the current shell."
 
 
 class WorkspaceProxy(QtCore.QObject):
-    def __init__(self, model):
+    
+    haveNewData = QtCore.pyqtSignal()
+    
+    def __init__(self):
         QtCore.QObject.__init__(self)
         
-        # Store model
-        self._model = model
-    
-    
-    def getWhos(self):
+        # Create timer to keep polling the process
+        self._timer = QtCore.QTimer(self)
+        self._timer.setInterval(2000)  # ms
+        self._timer.setSingleShot(True)
+        self._timer.timeout.connect(self.poll)
+        self._timer.start()
         
-        names = remoteEval('",".join(dir())')
-        names = names.split(',')
-        # Select names
-        names = [name for name in names if not name.startswith('__')]
-        # Get class and repr for all names at once
-        if names:
-            # Define list comprehensions (the one for the class is huge!)
-            nameString = ','.join(names)
-            classGetter = '[str(c) for c in '
-            classGetter += '[a[1] or a[0].__class__.__name__ for a in '
-            classGetter += '[(b, not hasattr(b,"__class__")) for b in [{}]'
-            classGetter += ']]]'
-            reprGetter = '[repr(name) for name in [{}]]'
-            #
-            classGetter = classGetter.format(nameString)
-            reprGetter = reprGetter.format(nameString)
-            # Use special seperator that is unlikely to be used, ever.
-            namesClass = remoteEval('"##IEP##".join({})'.format(classGetter))
-            namesRepr = remoteEval('"##IEP##".join({})'.format(reprGetter))
-            namesClass = namesClass.split('##IEP##')
-            namesRepr = namesRepr.split('##IEP##')
+        # Variables
+        self._variables = []
+    
+    
+    def poll(self):
+        shell = iep.shells.getCurrentShell()
+        if shell:
+            shell.postRequest('VARIABLES a', self.processResponse)
+        
+        # If no response poll again in 5 seconds
+        self._timer.start(5000)
+    
+    
+    def processResponse(self, response, id=None):
+        
+        # Poll next time
+        self._timer.start(2000)
+        
+        # Check
+        if not '##IEP##' in response:
+            print('Error getting VARIABLES:', response)
+            return
+        
+        # Store variables
+        self._variables = [r for r in response.split('##IEP##') if r]
+        
+        # Signal
+        self.haveNewData.emit()
+    
+#     def getWhos(self):
+#         
+#         names = remoteEval('",".join(dir())')
+#         names = names.split(',')
+#         # Select names
+#         names = [name for name in names if not name.startswith('__')]
+#         # Get class and repr for all names at once
+#         if names:
+#             # Define list comprehensions (the one for the class is huge!)
+#             nameString = ','.join(names)
+#             classGetter = '[str(c) for c in '
+#             classGetter += '[a[1] or a[0].__class__.__name__ for a in '
+#             classGetter += '[(b, not hasattr(b,"__class__")) for b in [{}]'
+#             classGetter += ']]]'
+#             reprGetter = '[repr(name) for name in [{}]]'
+#             #
+#             classGetter = classGetter.format(nameString)
+#             reprGetter = reprGetter.format(nameString)
+#             # Use special seperator that is unlikely to be used, ever.
+#             namesClass = remoteEval('"##IEP##".join({})'.format(classGetter))
+#             namesRepr = remoteEval('"##IEP##".join({})'.format(reprGetter))
+#             namesClass = namesClass.split('##IEP##')
+#             namesRepr = namesRepr.split('##IEP##')
 
 
-class WorkspaceModel(QtCore.QAbstractItemModel):
-    def __init__(self):
-        QtCore.QAbstractItemModel.__init__(self)
-    
-    def data(self, index, role):
-        if not index.isValid() or role not in [0, 8]:
-            return None
+class WorkspaceTree(QtGui.QTreeWidget):
+    def __init__(self, parent):
+        QtGui.QTreeWidget.__init__(self, parent)
         
-        # get menu or action item
-        item = index.internalPointer()
-        if index.column() == 0:
-            return 'aap'
-        if index.column() == 1:
-            return 'noot'
-        if index.column() == 2:
-            return 'mies'
-    
-    
-    def rowCount(self, parent):
-        if parent.isValid():
-            return 0
-        else:
-            return 4
-    
-    def columnCount(self, parent):
-        return 3
-    
-    
-    def headerData(self, section, orientation, role):
-        if role == 0:# and orientation==1:
-            tmp = ['Name','Type','Repr']
-            return tmp[section]
-    
-    
-    def parent(self, index):
-        if not index.isValid():
-            return QtCore.QModelIndex()
+        # Set header stuff
+        self.setHeaderHidden(False)
+        self.setColumnCount(3)
+        self.setHeaderLabels(['Name', 'Type', 'Repr'])
+        #self.setColumnWidth(0, 100)
+        self.setSortingEnabled(True)
         
-        return QtCore.QModelIndex()
+        # Nice rows
+        self.setAlternatingRowColors(True)
         
-#         item = index.internalPointer()
-#         pitem = item.parent()
-#         if pitem is self._root:
-#             return QtCore.QModelIndex()
-#         else:
-#             L = pitem.parent().actions()
-#             row = 0
-#             if pitem in L:
-#                 row = L.index(pitem)
-#             return self.createIndex(row, 0, pitem)
+        # Create proxy
+        self._proxy = WorkspaceProxy()
+        self._proxy.haveNewData.connect(self.fillWorkspace)
     
-    def index(self, row, column, parent):
-#         if not self.hasIndex(row, column, parent):
-#             return QtCore.QModelIndex()
-        # establish parent
-        return self.createIndex(row, column, )
+    
+    def fillWorkspace(self):
         
-#         if not parent.isValid():
-#             parentMenu = self._root
-#         else:
-#             parentMenu = parent.internalPointer()
-#         # produce index and make menu if the action represents a menu
-#         childAction = parentMenu.actions()[row]
-#         if childAction.menu():
-#             childAction = childAction.menu()        
-#         return self.createIndex(row, column, childAction)
-#         # This is the trick. The internal pointer is the way to establish
-#         # correspondence between ModelIndex and underlying data.
+        # Cleat first
+        self.clear()
+        
+        # Add elements
+        for des in self._proxy._variables:
+            
+            # Get parts
+            parts = des.split(',',4)
+            if len(parts) < 4:
+                continue
+            
+            # Create item
+            kind = parts.pop(2)
+            tt = '%s: %s' % (parts[0], parts[-1])
+            item = QtGui.QTreeWidgetItem(parts, 0)
+            item.setToolTip(0,tt)
+            item.setToolTip(1,tt)
+            item.setToolTip(2,tt)
+            self.addTopLevelItem(item)
     
+
 class IepWorkspace(QtGui.QWidget):
     def __init__(self, parent):
         QtGui.QWidget.__init__(self, parent)
         
-        # Create view and model
-        self._model = WorkspaceModel()
-        self._view = QtGui.QTreeView(self)
-        self._view.setModel(self._model)
+        self._tree = WorkspaceTree(self)
         
         # Set layout
         layout = QtGui.QVBoxLayout(self)
-        layout.addWidget(self._view)
+        layout.addWidget(self._tree)
         self.setLayout(layout)
     
