fe6b03ea26e06dc5ca05c6b48e52c54dd55130e7
Almar Klein <almarklein@users.noreply.github.com>
2010-11-17 23:56:10 +0100

    in interactive help. removed annoying single space indentation and removed display of objectname at all in the textfield (it is shown in the line-edit widget anyway. Made some further improvements for cases when docs are not available or the object does not exist.

--- iepRemote2.py
+++ iepRemote2.py
@@ -1112,10 +1112,9 @@ class IntroSpectionThread(threading.Thread):
             
             # build final text
             text = '\n'.join([objectName, h_class, h_fun, h_repr, h_text])
-        
+            
         except Exception:
-            text = "No help available."
-        
+            text = '\n'.join([objectName, '', '', '', 'No help available.'])
         
         # The lines below can be uncomented for debugging, but they don't
         # work on python < 2.6.
