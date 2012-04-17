6127c737ba51ac8e5649501bec024d1f53f3f543
Almar Klein <almarklein@users.noreply.github.com>
2012-04-17 15:51:33 +0200

    This is IEP 3.0.beta. (except for maybe small fixes that I found during freezing and testing)

--- iep.py
+++ iep.py
@@ -16,7 +16,7 @@ import ssdf  # import ssdf or the suplied copy if not available
 from PyQt4 import QtCore, QtGui
 
 # Set version number
-__version__ = '3.0.dev'
+__version__ = '3.0.beta'
 
 
 ## Define some functions
