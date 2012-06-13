4209a6f44993e37f209e5632dc06eebba2679be8
almar <almar@localhost>
2012-06-13 21:50:38 +0200

    Prefer PySide. We can encounter a couple of bugs, but it will give us a chance to solve them.

--- codeeditor/qt.py
+++ codeeditor/qt.py
@@ -1,14 +1,12 @@
 
 # Simple module to allow using both PySide and PyQt4
 
-# Try PyQt4 first, until IEP runs stable in PySide
 try:
-    from PyQt4 import QtCore, QtGui
-    QtCore.Signal = QtCore.pyqtSignal # Define signal as pyqtSignal
+    from PySide import QtCore, QtGui
 except ImportError:
     try:
-        from PySide import QtCore, QtGui
+        from PyQt4 import QtCore, QtGui
+        QtCore.Signal = QtCore.pyqtSignal # Define signal as pyqtSignal
     except ImportError:
         raise ImportError("Both PySide and PyQt4 could not be imported.")
 
-Qt = QtCore.Qt
