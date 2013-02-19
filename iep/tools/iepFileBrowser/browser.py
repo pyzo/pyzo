faaab2395bd3a0c92db6cd8bf29e0ca8fde72035
almar <almar@localhost>
2013-02-19 14:17:48 +0100

    FileBrowser: Improvements to functionality and looks+layout.

--- iep/tools/iepFileBrowser/browser.py
+++ iep/tools/iepFileBrowser/browser.py
@@ -27,13 +27,6 @@ class Browser(QtGui.QWidget):
         # Create star button
         self._projects = Projects(self)
         
-        # Create up button
-        self._up = QtGui.QToolButton(self)
-        self._up.setIcon( iep.icons.folder_parent )  # folder_parent bullet_arrow_up
-        self._up.setStyleSheet("QToolButton { padding: 0px; }");
-        #self._up.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
-        self._up.setIconSize(QtCore.QSize(18,18))
-        
         # Create path input/display lineEdit
         self._pathEdit = PathInput(self)
         
@@ -47,18 +40,21 @@ class Browser(QtGui.QWidget):
         
         # Create name filter
         self._nameFilter = NameFilter(self)
-        self._nameFilter.lineEdit().setToolTip('File filter pattern')  
+        #self._nameFilter.lineEdit().setToolTip('File filter pattern')  
+        self._nameFilter.setToolTip('Filename filter')  
+        self._nameFilter.setPlaceholderText(self._nameFilter.toolTip())
         
         # Create search filter
         self._searchFilter = SearchFilter(self)
-        self._searchFilter.setToolTip('Search pattern')
+        self._searchFilter.setToolTip('Search in files')
         self._searchFilter.setPlaceholderText(self._searchFilter.toolTip())
         
         # Signals to sync path. 
         # Widgets that can change the path transmit signal to _tree
+        self._pathEdit.dirUp.connect(self._tree.setFocus)
+        self._pathEdit.dirUp.connect(self._tree.setPathUp)
         self._pathEdit.dirChanged.connect(self._tree.setPath)
         self._projects.dirChanged.connect(self._tree.setPath)
-        self._up.clicked.connect(self._tree.setPathUp)
         #
         self._nameFilter.filterChanged.connect(self._tree.onChanged) # == update
         self._searchFilter.filterChanged.connect(self._tree.onChanged)
@@ -79,25 +75,15 @@ class Browser(QtGui.QWidget):
         layout.setContentsMargins(0,0,0,0)
         #layout.setSpacing(6)
         self.setLayout(layout)
-        #
-        subLayout = QtGui.QHBoxLayout()
-        subLayout.addWidget(self._up, 0)
-        subLayout.addStretch(1)
-        subLayout.addWidget(self._projects, 8)
-        layout.addLayout(subLayout)
-        #
-        #subLayout = QtGui.QHBoxLayout()
-        #subLayout.setSpacing(2)
-        #subLayout.addWidget(self._up)
+        #        
+        layout.addWidget(self._projects)
         layout.addWidget(self._pathEdit)
-        #layout.addLayout(subLayout)
-        #
         layout.addWidget(self._tree)
         #
         subLayout = QtGui.QHBoxLayout()
         subLayout.setSpacing(2)
         subLayout.addWidget(self._nameFilter, 5)
-        subLayout.addWidget(self._searchFilter, 4)
+        subLayout.addWidget(self._searchFilter, 5)
         layout.addLayout(subLayout)
     
     def closeEvent(self, event):
@@ -105,9 +91,9 @@ class Browser(QtGui.QWidget):
         super().closeEvent(event)
         self._fsProxy.stop()
     
-    
     def nameFilter(self):
-        return self._nameFilter.lineEdit().text()
+        #return self._nameFilter.lineEdit().text()
+        return self._nameFilter.text()
     
     def searchFilter(self):
         return {'pattern': self._searchFilter.text(),
@@ -116,20 +102,17 @@ class Browser(QtGui.QWidget):
                 'subDirs': self.config.searchSubDirs,
                 }
     
-   
     @property
     def expandedDirs(self):
         """ The list of the expanded directories. 
         """
         return self.parent().config.expandedDirs
     
-    
     @property
     def starredDirs(self):
         """ A list of the starred directories (not a copy).
         """
         return [d.path for d in self.parent().config.starredDirs]
-    
     
     def test(self, sort=False):
         items = []
@@ -147,7 +130,186 @@ class Browser(QtGui.QWidget):
     
 
 
-class PathInput(QtGui.QLineEdit):
+class LineEditWithToolButtons(QtGui.QLineEdit):
+    """ Line edit to which tool buttons (with icons) can be attached.
+    """
+    
+    def __init__(self, parent):
+        QtGui.QLineEdit.__init__(self, parent)
+        self._leftButtons = []
+        self._rightButtons = []
+    
+    def addButtonLeft(self, icon, willHaveMenu=False):
+        return self._addButton(icon, willHaveMenu, self._leftButtons)
+    
+    def addButtonRight(self, icon, willHaveMenu=False):
+        return self._addButton(icon, willHaveMenu, self._rightButtons)
+    
+    def _addButton(self, icon, willHaveMenu, L):
+        # Create button
+        button = QtGui.QToolButton(self)
+        L.append(button)
+        # Customize appearance
+        button.setIcon(icon)
+        button.setIconSize(QtCore.QSize(16,16))
+        button.setStyleSheet("QToolButton { border: none; padding: 0px; }")        
+        #button.setStyleSheet("QToolButton { border: none; padding: 0px; background-color:red;}");
+        # Set behavior
+        button.setCursor(QtCore.Qt.ArrowCursor)
+        button.setPopupMode(button.InstantPopup)
+        # Customize alignment
+        if willHaveMenu and sys.platform.startswith('win'):
+            button.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+            button.setText(' ')
+        # Update self
+        self._updateGeometry()
+        return button
+    
+    def setButtonVisible(self, button, visible):
+        for but in self._leftButtons:
+            if but is button:
+                but.setVisible(visible)
+        for but in self._rightButtons:
+            if but is button:
+                but.setVisible(visible)
+        self._updateGeometry()
+    
+    def resizeEvent(self, event):
+        QtGui.QLineEdit.resizeEvent(self, event)
+        self._updateGeometry(True)
+    
+    def showEvent(self, event):
+        QtGui.QLineEdit.showEvent(self, event)
+        self._updateGeometry()
+    
+    def _updateGeometry(self, light=False):
+        if not self.isVisible():
+            return
+        
+        # Init
+        rect = self.rect()
+        
+        # Determine padding and height
+        paddingLeft, paddingRight, height = 1, 1, 0
+        #
+        for but in self._leftButtons:
+            if but.isVisible():
+                sz = but.sizeHint()
+                height = max(height, sz.height())
+                but.move(   1+paddingLeft,
+                            (rect.bottom() + 1 - sz.height())/2 )
+                paddingLeft += sz.width() + 1
+        #
+        for but in self._rightButtons:
+            if but.isVisible():
+                sz = but.sizeHint()
+                paddingRight += sz.width() + 1
+                height = max(height, sz.height())
+                but.move(   rect.right()-1-paddingRight, 
+                            (rect.bottom() + 1 - sz.height())/2 )
+        
+        # Set padding
+        ss = "QLineEdit { padding-left: %ipx; padding-right: %ipx} "
+        self.setStyleSheet( ss % (paddingLeft, paddingRight) );
+        
+        # Set minimum size
+        if not light:
+            fw = QtGui.qApp.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+            msz = self.minimumSizeHint()
+            w = max(msz.width(), paddingLeft + paddingRight + 10)
+            h = max(msz.height(), height + fw*2 + 2)
+            self.setMinimumSize(w,h)
+
+
+
+class PathInput(LineEditWithToolButtons):
+    """ Line edit for selecting a path.
+    """
+    
+    dirChanged = QtCore.Signal(Path)  # Emitted when the user changes the path (and is valid)
+    dirUp = QtCore.Signal()  # Emitted when user presses the up button
+    
+    def __init__(self, parent):
+        LineEditWithToolButtons.__init__(self, parent)
+        
+        # Create up button
+        self._upBut = self.addButtonLeft(iep.icons.folder_parent)
+        self._upBut.clicked.connect(self.dirUp)
+        
+        # To receive focus events
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        
+        # Set completion mode
+        self.setCompleter(QtGui.QCompleter())
+        c = self.completer()
+        c.setCompletionMode(c.InlineCompletion)
+        
+        # Set dir model to completer
+        dirModel = QtGui.QDirModel(c)
+        dirModel.setFilter(QtCore.QDir.Dirs | QtCore.QDir.NoDotAndDotDot)
+        c.setModel(dirModel)
+        
+        # Connect signals
+        #c.activated.connect(self.onActivated)
+        self.textEdited.connect(self.onTextEdited)
+        #self.textChanged.connect(self.onTextEdited)
+        #self.cursorPositionChanged.connect(self.onTextEdited)
+    
+    def setPath(self, path):
+        """ Set the path to display. Does nothing if this widget has focus.
+        """
+        if not self.hasFocus():
+            self.setText(path)
+            self.checkValid() # Reset style if it was invalid first
+    
+    
+    def checkValid(self):
+        # todo: This kind of violates the abstraction of the file system
+        # ok for now, but we should find a different approach someday
+        # Check
+        text = self.text()
+        dir = Path(text)
+        isvalid = text and dir.isdir and os.path.isabs(dir)
+        # Apply styling
+        ss = self.styleSheet().replace('font-style:italic; ', '')
+        if not isvalid:
+            ss = ss.replace('QLineEdit {', 'QLineEdit {font-style:italic; ')
+        self.setStyleSheet(ss)
+        # Return
+        return isvalid
+    
+    
+    def event(self, event):
+        # Capture key events to explicitly apply the completion and
+        # invoke checking whether the current text is a valid directory.
+        if isinstance(event, QtGui.QKeyEvent):
+            qt = QtCore.Qt
+            if event.key() in [qt.Key_Tab, qt.Key_Enter, qt.Key_Return]:
+                self.setText(self.text()) # Apply completion
+                self.onTextEdited() # Check if this is a valid dir
+                return True
+        return super().event(event)
+    
+    
+    def onTextEdited(self, dummy=None):
+        text = self.text()
+        if self.checkValid():            
+            self.dirChanged.emit(Path(text))
+    
+    
+    def focusOutEvent(self, event=None):
+        """ focusOutEvent(event)
+        On focusing out, make sure that the set path is correct.
+        """
+        if event is not None:
+            QtGui.QLineEdit.focusOutEvent(self, event)
+        
+        path = self.parent()._tree.path()
+        self.setPath(path)
+
+
+
+class _PathInput(QtGui.QLineEdit):
     """ Line edit for selecting a path.
     """
     
@@ -256,6 +418,10 @@ class Projects(QtGui.QWidget):
         self._menu = QtGui.QMenu(self._but)
         self._menu.triggered.connect(self.onMenuTriggered)
         self.buildMenu()
+        
+        # Make equal height
+        h = max(self._combo.sizeHint().height(), self._but.sizeHint().height())
+        self._combo.setMinimumHeight(h);  self._but.setMinimumHeight(h)
         
         # Connect signals
         self._but.pressed.connect(self.onButtonPressed)
@@ -398,32 +564,31 @@ class Projects(QtGui.QWidget):
 
 
 
-class NameFilter(QtGui.QComboBox):
+class NameFilter(LineEditWithToolButtons):
     """ Combobox to filter by name.
     """
     
     filterChanged = QtCore.Signal()
     
     def __init__(self, parent):
-        QtGui.QComboBox.__init__(self, parent)
+        LineEditWithToolButtons.__init__(self, parent)
         
-        # Set properties
-        self.setEditable(True)
-        self.setCompleter(None)
-        self.setInsertPolicy(self.NoInsert)
-        self.setMinimumSize(50, self.sizeHint().height())
-        
+        # Create tool button, and attach the menu
+        self._menuBut = self.addButtonRight(iep.icons['filter'], True)
+        self._menu = QtGui.QMenu(self._menuBut)
+        self._menu.triggered.connect(self.onMenuTriggered)
+        self._menuBut.setMenu(self._menu)
+        #
         # Add common patterns
         for pattern in ['*', '!*.pyc', 
                         '*.py *.pyw', '*.py *.pyw *.pyx *.pxd', 
                         '*.h *.c *.cpp']:
-            self.addItem(pattern)
+            self._menu.addAction(pattern)
         
         # Emit signal when value is changed
         self._lastValue = ''
-        self.lineEdit().editingFinished.connect(self.checkFilterValue)
-        self.lineEdit().returnPressed.connect(self.checkFilterValue)
-        self.activated.connect(self.checkFilterValue)
+        self.returnPressed.connect(self.checkFilterValue)
+        self.editingFinished.connect(self.checkFilterValue)
         
         # Ensure the namefilter is in the config and initialize 
         config = self.parent().config
@@ -431,70 +596,56 @@ class NameFilter(QtGui.QComboBox):
             config.nameFilter = '!*.pyc'
         self.setText(config.nameFilter)
     
-    def setText(self, value):
+    def setText(self, value, test=False):
         """ To initialize the name filter.
         """ 
-        self.lineEdit().setText(value)
+        QtGui.QLineEdit.setText(self, value)
+        if test:
+            self.checkFilterValue()
         self._lastValue = value
     
     def checkFilterValue(self):
-        value = self.lineEdit().text()
+        value = self.text()
         if value != self._lastValue:
             self.parent().config.nameFilter = value
             self._lastValue = value
             self.filterChanged.emit()
+    
+    def onMenuTriggered(self, action):
+        self.setText(action.text(), True)
 
 
-class SearchFilter(QtGui.QLineEdit):
+
+class SearchFilter(LineEditWithToolButtons):
     """ Line edit to do a search in the files.
     """ 
-    
-    OFFSET = 0
     
     filterChanged = QtCore.Signal()
     
     def __init__(self, parent):
-        QtGui.QLineEdit.__init__(self, parent)
+        LineEditWithToolButtons.__init__(self, parent)
         
-        # Load two icons
-        self._defaultIcon = iep.icons['magnifier']
-        self._clearIcon = iep.icons['cancel']
-        
-        # Create tool button
-        self._button = button = QtGui.QToolButton(self)
-        button.setIcon(self._defaultIcon)
-        button.setIconSize(QtCore.QSize(16,16))
-        button.setCursor(QtCore.Qt.ArrowCursor)
-        button.setStyleSheet("QToolButton { border: none; padding: 0px; }");
-        #button.setStyleSheet("QToolButton { border: none; padding: 0px; background-color:red;}");
-        button.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
-        button.setPopupMode(button.InstantPopup)
-        #
-        self._menu = QtGui.QMenu(button)
+        # Create tool button, and attach the menu
+        self._menuBut = self.addButtonRight(iep.icons['magnifier'], True)
+        self._menu = QtGui.QMenu(self._menuBut)
         self._menu.triggered.connect(self.onMenuTriggered)
-        button.setMenu(self._menu)
+        self._menuBut.setMenu(self._menu)
         self.buildMenu()
         
+        # Create cancel button
+        self._cancelBut = self.addButtonRight(iep.icons['cancel'])
+        self._cancelBut.setVisible(False)
+        
+        # Keep track of last value of search (initialized empty)
+        self._lastValue = '' 
+        
         # Connect signals
-        button.pressed.connect(self.onButtonPressed)
-        self.textChanged.connect(self.updateCloseButton)                
-        
-        # Set padding of line edit
-        fw = QtGui.qApp.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        padding =  button.sizeHint().width() + fw + 1 + self.OFFSET
-        self.setStyleSheet("QLineEdit { padding-right: %ipx; } " % padding);
-        
-        # Set minimum size
-        msz = self.minimumSizeHint()
-        w = max(msz.width(), button.sizeHint().width()*2 + fw * 2 + 2 + self.OFFSET)
-        h = max(msz.height(), button.sizeHint().height() + fw * 2 + 2)
-        self.setMinimumSize(w,h)
-        
-        self._lastValue = '' # The search filer is always initialized empty
-        self.returnPressed.connect(self.checkFilterValue)
+        self._cancelBut.pressed.connect(self.onCancelPressed)
+        self.textChanged.connect(self.updateCancelButton)        
         self.editingFinished.connect(self.checkFilterValue)
+        self.returnPressed.connect(self.forceFilterChanged)
     
-    def onButtonPressed(self):
+    def onCancelPressed(self):
         """ Clear text or build menu.
         """
         if self.text():
@@ -509,22 +660,13 @@ class SearchFilter(QtGui.QLineEdit):
             self._lastValue = value
             self.filterChanged.emit()
     
-    def resizeEvent(self, event):
-        QtGui.QLineEdit.resizeEvent(self, event)
-        
-        sz = self._button.sizeHint()
-        fw = QtGui.qApp.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        rect = self.rect()
-        self._button.move(  rect.right() - fw -sz.width() - self.OFFSET,
-                            (rect.bottom() + 1 - sz.height())/2)
+    def forceFilterChanged(self):
+        self._lastValue = value = self.text()
+        self.filterChanged.emit()
     
-    def updateCloseButton(self, text):
-        if self.text():
-            self._button.setIcon(self._clearIcon)
-            self._button.setMenu(None)
-        else:
-            self._button.setIcon(self._defaultIcon)
-            self._button.setMenu(self._menu)
+    def updateCancelButton(self, text):
+        visible = bool(self.text())
+        self.setButtonVisible(self._cancelBut, visible)
     
     def buildMenu(self):
         config = self.parent().config
@@ -558,3 +700,5 @@ class SearchFilter(QtGui.QLineEdit):
             config[option] = not config[option]
         else:
             config[option] = True
+        # Update
+        self.filterChanged.emit()
