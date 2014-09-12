758590ac4f64f5d070ba9ceccf7d30c654d4318d
Windel Bouwman <windelbouwman@users.noreply.github.com>
2014-09-12 11:18:35 +0200

    Added my name as author

--- iep/tools/iepAssistant.py
+++ iep/tools/iepAssistant.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2013, the IEP development team
+# Author: Windel Bouwman
 #
 # IEP is distributed under the terms of the (new) BSD License.
 # The full license can be found in 'license.txt'.
