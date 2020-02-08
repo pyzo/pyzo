e7a1719409cbc07d219a86fe870d7bd3716145c0
Yann Salmon <nomail@yannsalmon.fr>
2020-02-08 08:57:00 +0100

    use already existing header code to display signature

--- pyzo/tools/pyzoInteractiveHelp.py
+++ pyzo/tools/pyzoInteractiveHelp.py
@@ -670,6 +670,9 @@ class PyzoInteractiveHelp(QtWidgets.QWidget):
                         header, sep, docs = h_text.partition("\n")
                         # h_text = header + '\n\n' + docs
                         h_text = docs
+                    elif h_fun is not None and h_fun != "" :
+                        header = h_fun
+
 
                 # Parse the text as rest/numpy like docstring
                 h_text = self.smartFormat(h_text)
@@ -686,7 +689,7 @@ class PyzoInteractiveHelp(QtWidgets.QWidget):
 
             # Compile rich text
             text += get_title_text(objectName, h_class, h_repr)
-            if h_fun is not None and h_fun != "" :
+            if not self._config.smartNewlines and h_fun is not None and h_fun != "" :
                 text += "<p><b>Signature:</b> {}</p>".format(h_fun)
             text += "{}<br />".format(h_text)
 
