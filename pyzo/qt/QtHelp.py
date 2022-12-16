4cbc02f20bb7473da50f36886a416535331d7209
Almar Klein <almar@almarklein.org>
2022-12-16 23:07:44 +0100

    remove code from QtHelp

--- pyzo/qt/QtHelp.py
+++ pyzo/qt/QtHelp.py
@@ -20,10 +20,3 @@ elif PYSIDE2:
     from PySide2.QtHelp import *
 else:
     raise PythonQtError("No Qt bindings could be found")
-
-
-from .enumfixer import fix_enums
-
-for ob in list(globals().values()):
-    if isinstance(ob, type) and ob.__name__.startswith("Q"):
-        fix_enums(ob)
