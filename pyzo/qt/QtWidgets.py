5ad31845870a776d2e55b678dd3b411ea4dd6d06
bdieterm <119257544+bdieterm@users.noreply.github.com>
2023-05-09 12:45:47 +0200

    fixes for PyQt6 support and running Pyzo in Pyzo

--- pyzo/qt/QtWidgets.py
+++ pyzo/qt/QtWidgets.py
@@ -12,23 +12,27 @@ from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError
 
 
 if PYQT6:
-    from PyQt6 import QtWidgets
     from PyQt6.QtWidgets import *
     from PyQt6.QtGui import QAction, QActionGroup, QShortcut, QFileSystemModel
     from PyQt6.QtOpenGLWidgets import QOpenGLWidget
 
     # Map missing/renamed methods
     import inspect
-    QTextEdit.setTabStopWidth = inspect.getattr_static(QTextEdit, 'setTabStopDistance')
-    QTextEdit.tabStopWidth = inspect.getattr_static(QTextEdit, 'tabStopDistance')
-    QTextEdit.print_ = inspect.getattr_static(QTextEdit, 'print')
-    QPlainTextEdit.setTabStopWidth = inspect.getattr_static(QPlainTextEdit, 'setTabStopDistance')
-    QPlainTextEdit.tabStopWidth = inspect.getattr_static(QPlainTextEdit, 'tabStopDistance')
-    QPlainTextEdit.print_ = inspect.getattr_static(QPlainTextEdit, 'print')
-    QApplication.exec_ = inspect.getattr_static(QApplication, 'exec')
-    QDialog.exec_ = inspect.getattr_static(QDialog, 'exec')
-    QMenu.exec_ = inspect.getattr_static(QMenu, 'exec')
+    QTextEdit.setTabStopWidth = inspect.getattr_static(QTextEdit, "setTabStopDistance")
+    QTextEdit.tabStopWidth = inspect.getattr_static(QTextEdit, "tabStopDistance")
+    QTextEdit.print_ = inspect.getattr_static(QTextEdit, "print")
+    QPlainTextEdit.setTabStopWidth = inspect.getattr_static(QPlainTextEdit, "setTabStopDistance")
+    QPlainTextEdit.tabStopWidth = inspect.getattr_static(QPlainTextEdit, "tabStopDistance")
+    QPlainTextEdit.print_ = inspect.getattr_static(QPlainTextEdit, "print")
 
+    if QApplication.__name__ != "QApplication_hijacked":
+        QApplication.exec_ = inspect.getattr_static(QApplication, "exec")
+    else:
+        print("Pyzo is executed inside another Pyzo instance")
+    QDialog.exec_ = inspect.getattr_static(QDialog, "exec")
+    QMenu.exec_ = inspect.getattr_static(QMenu, "exec")
+
+    from PyQt6 import QtWidgets
     from .enums_compat import promote_enums
     promote_enums(QtWidgets)
     del QtWidgets
@@ -36,7 +40,6 @@ if PYQT6:
 elif PYQT5:
     from PyQt5.QtWidgets import *
 elif PYSIDE6:
-    from PySide6 import QtWidgets
     from PySide6.QtWidgets import *
     from PySide6.QtGui import QAction, QActionGroup, QShortcut
     from PySide6.QtOpenGLWidgets import QOpenGLWidget
@@ -48,10 +51,14 @@ elif PYSIDE6:
     QPlainTextEdit.tabStopWidth = QPlainTextEdit.tabStopDistance
 
     # Map DeprecationWarning methods
-    QApplication.exec_ = QApplication.exec
+    if QApplication.__name__ != "QApplication_hijacked":
+        QApplication.exec_ = QApplication.exec
+    else:
+        print("Pyzo is executed inside another Pyzo instance")
     QDialog.exec_ = QDialog.exec
     QMenu.exec_ = QMenu.exec
 
+    from PySide6 import QtWidgets
     from .enums_compat import promote_enums
     promote_enums(QtWidgets)
     del QtWidgets
